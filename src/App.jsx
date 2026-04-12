71158d173ec6f13bd177f3815f04fc2dc40b0f0d
Federico Celia <73991066+federicocelia@users.noreply.github.com>
2026-04-12 02:38:26 +0100

    Add initial project structure and Vite configuration

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import About from "./About.jsx";
+import Interests from "./Interests.jsx";
+import Footer from "./Footer.jsx";
+import Info from "./Info.jsx";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Info />
+      <About />
+      <Interests />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
